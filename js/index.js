b7b2964411412a9b7fe9d5a3c2428bc20d68a065
Anwesh17 <anweshthecool0@gmail.com>
2016-01-01 00:24:55 +0530

    7th commit

--- js/index.js
+++ js/index.js
@@ -5,14 +5,13 @@ window.onload = function() {
     canvas = document.getElementById('canv_container');
     context = canvas.getContext('2d');
     var worker = new Worker('CanvWorker.js');
-    worker.onmessage = function(event) {
+    setInterval(function(){
         context.clearRect(0,0,1000,600);
-        console.log(rects);
         rects.forEach(function(rect,index){
             rect.draw(context);
             rect.move();
         });
-    }
+    },100);
 };
 window.onmousedown = function(event) {
     rects.push({x:event.pageX,y:event.pageY,deg:0,rot:0,draw:function(ctx){ctx.save();ctx.translate(this.x,this.y);ctx.rotate(this.deg*Math.PI/180);ctx.fillRect(-50,-50,100,100);ctx.restore();},move:function(){this.deg+=10;}});
